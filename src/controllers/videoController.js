49ff96a320ae6059ef22f21e08fdca40ecc6fb10
JsKim <75792069+Junseok3608@users.noreply.github.com>
2023-03-01 15:17:36 +0900

    ADD: video upload

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -53,6 +53,15 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload" });
 };
 export const postUpload = (req, res) => {
-  //여기에서 video를 videos array에 추가할 거임
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: "just now",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
   return res.redirect("/");
 };
